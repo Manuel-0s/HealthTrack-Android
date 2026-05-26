12baa23cf2543489cf9d16a7de3c6b94e9946739
Manuel <manuel020f@gmail.com>
2026-05-26 08:58:29 -0600

    Estandarizacion de valores

--- app/src/main/java/com/example/healthtrack/domain/model/MetricsField.kt
+++ app/src/main/java/com/example/healthtrack/domain/model/MetricsField.kt
@@ -1,11 +1,11 @@
 package com.example.healthtrack.domain.model
 
 enum class MetricsField(val label: String) {
-    FREQUENCY("Frecuencia cardiaca"),
+    FREQUENCY("Frecuencia Cardíaca"),
     GLUCOSE("Glucosa"),
     BLOOD_PRESSURE("Presion Arterial"),
-    SYSTOLIC_PRESSURE("Presion sistolica"),
-    DIASTOLIC_PRESSURE("Presion diastolica"),
+    SYSTOLIC_PRESSURE("Presión"),
+    DIASTOLIC_PRESSURE("Presión diastólica"),
 
     IMC("IMC"),
     WEIGHT("Peso"),
